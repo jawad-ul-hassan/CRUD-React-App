1401304fd6eba2a237b13d515dd96d624a36abac
Jawad Ul Hassan <intelligentjawad@gmail.com>
2021-05-30 04:59:46 +0500

    Delete functionality added

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -2,11 +2,16 @@ import React from 'react';
 import styles from './UserCard.module.css';
 
 const UserCard = props => {
+  const deleteCard = () => {
+    props.deleteHandler(props.id);
+  };
+
   return (
     <div className={styles['user-card']}>
       <p>
         <span>Name:</span> {props.name} & <span>Age:</span> {props.age}
       </p>
+      <button onClick={deleteCard}>Delete</button>
     </div>
   );
 };
